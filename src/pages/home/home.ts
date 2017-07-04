064ee1e950e966d708a3e4b0bd86f667cc79d765
Alexis <adoualle@gmail.com>
2017-07-04 18:30:37 +0200

    works

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,66 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
+
 })
 export class HomePage {
+  public players: any;
 
-  constructor(public navCtrl: NavController) {
+  public tap: number = 0;
+  constructor(public alertCtrl: AlertController) {
+    this.players = [{"prenomJoueur":"Alexis", "score":0},
+                    {"prenomJoueur":"Matthias", "score":0},
+                    {"prenomJoueur":"Hugo", "score":0},
+                    {"prenomJoueur":"Paul", "score":0},
+                    {"prenomJoueur":"JB", "score":0}
+                  ];
 
+  }
+
+  ajouterJoueur() {
+    let prompt = this.alertCtrl.create({
+     title: 'Nouveau joueur',
+     message: "Entrez le prénom:",
+     inputs: [
+       {
+         name: 'prenomJoueur',
+         placeholder: 'Prénom'
+       },
+     ],
+     buttons: [
+       {
+         text: 'Cancel',
+         handler: data => {
+           console.log('Cancel clicked');
+         }
+       },
+       {
+         text: 'Save',
+         handler: data => {
+           this.players.push({"prenomJoueur":prompt.data.inputs[0].value, "score":0});
+         }
+       }
+     ]
+   });
+   prompt.present();
+
+  }
+
+  supprimerJoueur(idJoueur) {
+    this.players.splice(idJoueur,1);
+
+  }
+
+  incrementer(idJoueur) {
+    this.players[idJoueur].score += 1;
+  }
+
+  decrementer(idJoueur) {
+    this.players[idJoueur].score -= 1;
   }
 
 }
