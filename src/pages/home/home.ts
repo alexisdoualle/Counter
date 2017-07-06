6c9608f27bd5f8c5527509b4a2c29764b9807e6a
Alexis <adoualle@gmail.com>
2017-07-06 14:34:30 +0200

    reorder function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,6 +9,8 @@ import { AlertController } from 'ionic-angular';
 })
 export class HomePage {
   public players: any;
+  public round: any;
+  public reorderBool: any;
 
   public tap: number = 0;
   constructor(public alertCtrl: AlertController) {
@@ -20,6 +22,8 @@ export class HomePage {
                     {"prenomJoueur":"Blandine", "score":0},
                     {"prenomJoueur":"Gaspard", "score":0},
                   ];
+    this.round = 0;
+    this.reorderBool = false;
 
   }
 
@@ -57,12 +61,56 @@ export class HomePage {
 
   }
 
+  changerNom(idJoueur) {
+    let prompt = this.alertCtrl.create({
+     title: 'Nouveau prénom',
+     message: "Entrez le prénom:",
+     inputs: [
+       {
+         name: 'nouveauNom',
+         placeholder: 'Prénom'
+       },
+     ],
+     buttons: [
+       {
+         text: 'Cancel',
+         handler: data => {
+           console.log('Cancel clicked');
+         }
+       },
+       {
+         text: 'Save',
+         handler: data => {
+         this.players[idJoueur].prenomJoueur = prompt.data.inputs[0].value;
+         }
+       }
+     ]
+    });
+    prompt.present();
+
+  }
+
   incrementer(idJoueur) {
     this.players[idJoueur].score += 1;
   }
 
+  incrementerBeaucoup(idJoueur) {
+    this.players[idJoueur].score += 10;
+  }
+
+
   decrementer(idJoueur) {
     this.players[idJoueur].score -= 1;
+  }
+
+  decrementerBeaucoup(idJoueur) {
+    this.players[idJoueur].score -= 10;
+  }
+
+  reorderItems(indexes) {
+    let element = this.players[indexes.from];
+    this.players.splice(indexes.from, 1);
+    this.players.splice(indexes.to, 0, element);
   }
 
 }
